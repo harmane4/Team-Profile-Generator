272f9d8ac74d66d3c8563a8442788056aa3717e8
Elise Harman <elise_harman@hotmail.com>
2021-05-07 12:02:50 +0800

    Two tests passing for employee object and name but need to add error handling

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,23 @@
-describe(`my-function`, () => {
-    it(`does nothing right now... ;)`, () => {
-        
-    })
-})
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    // Error Handling 
+
+    // Employee Object
+    it("Can generate a new employee object" , () => {
+        const employee =  new Employee();  
+        const newEmployee = typeof(employee);
+        expect(newEmployee).toEqual("object"); 
+    });
+
+    // Employee Name
+    it("Generate name property when getName() is invoked" , () => {
+        const employee = new Employee({
+            id: 1 ,
+            name: "Bob D" ,
+            email: "bob@emloyee.net.au" ,
+        });
+        const employeeName = employee.getName();
+        expect(employeeName).toEqual(employeeName);
+    });
+});
