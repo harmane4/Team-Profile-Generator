116484a48b97bf9976ccd4d9a659e91685e734bb
Elise Harman <elise_harman@hotmail.com>
2021-05-08 16:58:34 +0800

    Added inquier questions

---
+++ inquierer.js
@@ -0,0 +1,56 @@
+// Packages needed for this application
+const inquirer = require('inquirer'); 
+const fs = require('fs');
+
+
+// Questions array containing question object
+const teamMember = [
+    {
+        type: 'input' ,
+        name: 'name' ,
+        message: "Please enter team member's name:" ,
+    },
+    {
+        type: 'list' , 
+        name: 'role' , 
+        message: "Please select team member's role:" ,
+        choices: ['Manager', 'Engineer','Intern'],
+    },
+    {
+        type: 'input' ,
+        name: 'ID' , 
+        message: "Please enter team member's ID number:" ,
+    },
+    {
+        type: 'input' , 
+        name: 'email' , 
+        message: "Please enter team member's email:" ,
+    },
+    {
+        type: 'confirm' , 
+        name: 'add' , 
+        message: "Would you like to add another team member?" ,
+        // If statement here
+    },
+   
+]
+
+// // WRITE INFORMATION TO A FILE
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, (err) => 
+//     err ? console.log(err) : console.log('README.md file created')
+//     );
+// }
+
+// const fileName = 'README.md';
+
+// // TODO: Create a function to initialize app
+// function init() {
+// inquirer.prompt(teamMember).then((data) => {
+// let completedTemplate = generateMarkdown(data);
+// writeToFile(fileName, completedTemplate)
+// })
+// } 
+
+// // Function call to initialize app
+// init();
