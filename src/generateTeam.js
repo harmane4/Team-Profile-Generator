2340bb74dd91fbb23c08a93570b7878764c11f88
Elise Harman <elise_harman@hotmail.com>
2021-05-14 12:14:14 +0800

    Completed switch statement, invoked getOfficeNumber() + getGitHub() + getSchool(), exported module

---
+++ src/generateTeam.js
@@ -0,0 +1,114 @@
+
+function generateTeamMemberCards(employee) {
+   return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Generator</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    </head>
+    <body>
+      
+        <section class="hero is-link">
+            <div class="hero-body">
+              <p class="title has-text-centered">
+                My Team
+              </p>
+            </div>
+          </section>
+    
+          <div class="column is-one-third is-multiline">
+            <div class="box">
+            ${generateTeamMemberCard(employee)}
+            </div>
+          </div>
+    </body>
+    </html>`
+}
+
+function generateTeamMemberCard(employee) {
+   
+  return employee.map(function(e) {
+   let role = e.getRole()
+   console.log("role" , role)
+    switch (role) {
+      case "Manager":
+        return generateManagerCard(e)
+        break;
+      case "Engineer":
+        return generateEngineerCard(e)
+        break;
+        case "Intern":
+          return generateInternCard(e)
+    }
+    console.log(e)
+  }).join("")
+}
+
+function generateManagerCard(manager) {
+   return ` <ul>
+            <div class="box">
+            <li>${manager.name}</li>
+            <br>
+            <li>${manager.getRole()}</li>
+          </div>
+          <div class="box">
+            <li>ID: ${manager.id}</li>
+          </div>
+            <div class="box">
+              <li>Email: ${manager.email}</li>
+            </div>
+            <div class="box">
+              <li>Office Number: ${manager.getOfficeNumber()}</li>
+            </div>
+          </ul>
+        </div>
+      </div>`
+}
+
+function generateEngineerCard(engineer) {
+    return ` <ul>
+    <div class="box">
+    <li>${engineer.name}</li>
+    <br>
+    <li>${engineer.getRole()}</li>
+  </div>
+  <div class="box">
+    <li>ID: ${engineer.id}</li>
+  </div>
+    <div class="box">
+      <li>Email: ${engineer.email}</li>
+    </div>
+    <div class="box">
+      <li>Github: ${engineer.getGitHub()}</li>
+    </div>
+  </ul>
+</div>
+</div>`
+}
+
+function generateInternCard(intern) {
+    return `<ul>
+    <div class="box">
+    <li>${intern.name}</li>
+    <br>
+    <li>${intern.getRole()}</li>
+  </div>
+  <div class="box">
+    <li>ID:${intern.id}</li>
+  </div>
+    <div class="box">
+      <li>Email:${intern.email}</li>
+    </div>
+    <div class="box">
+      <li>School:${intern.getSchool()}</li>
+    </div>
+  </ul>
+</div>
+</div>`
+}
+
+
+module.exports = generateTeamMemberCards
